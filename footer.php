9765c108106e847a928c61a016ecc6438d12813d
Trinh Minh Duc <21111062080@hunre.edu.vn>
2024-04-19 14:15:03 +0700

    shopmypham
    upload

---
+++ footer.php
@@ -0,0 +1,151 @@
+<footer>
+    <div class="footer-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4">
+                    <div class="footer-contact">
+                        <img src="#" alt="">
+                        <p>Trang web được xây dựng bởi Nhóm 9, dưới đây là thông tin liên lạc: </p>
+                        <ul class="address">
+
+                            <li>
+                                <span class="fa fa-phone"></span>
+                                123 456 789
+                            </li>
+                            <li>
+                                <span class="fa fa-github"></span>
+                                github.com
+                            </li>
+                            <li>
+                                <span class="fa fa-envelope-o"></span>
+                                123 456 789
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 hidden-sm">
+                    <div class="footer-tweets">
+                        <div class="footer-title">
+                            <h3>Mô tả</h3>
+                        </div>
+                        <div class="twitter-text" style="float:left;">
+                            <p>Đây là trang web bán quần áo mỹ phẩm dựa trên trang web https://dirtycoins.vn/ được
+                                tạo ra nhằm phục vụ cho môn học Phát triển phần mềm mã nguồn mở, Dưới đây là link source
+                                code</p>
+                            <a
+                                href="https://github.com/Dung-Vu/-sell-men-s-clothes">https://github.com/Dung-Vu/-sell-men-s-clothes</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4">
+                    <div class="footer-support">
+                        <div class="footer-title">
+                            <h3>Họ và tên thành viên nhóm</h3>
+                        </div>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">Trịnh Minh Đức</a></li>
+                                <li><a href="#">Nguyễn Trung Hiếu</a></li>
+                                <li><a href="#">Đường Đình Hiếu</a></li>
+                                <li><a href="#">Tạ Văn Thành</a></li>
+                                <li><a href="#">Vũ Lê Thái Hà</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4">
+                    <div class="footer-info" style="width: 170px;">
+                        <div class="footer-title">
+                            <h3>Mã số sinh viên</h3>
+                        </div>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">311 941 0032</a></li>
+                                <li><a href="#">311 941 0032</a></li>
+                                <li><a href="#">311 941 0061</a></li>
+                                <li><a href="#">311 941 0067</a></li>
+                                <li><a href="#">311 941 0080</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer top area end -->
+    <!-- footer area start -->
+
+    <a href="#" id="scrollUp"><i class="fa fa fa-arrow-up"></i></a>
+</footer>
+<script src="js/vendor/jquery-1.12.1.min.js"></script>
+<!-- bootstrap JS
+        ============================================ -->
+<script src="js/bootstrap.min.js"></script>
+<!-- wow JS
+        ============================================ -->
+<script src="js/wow.min.js"></script>
+<!-- price-slider JS
+        ============================================ -->
+<script src="js/jquery-price-slider.js"></script>
+<!-- nivoslider JS
+        ============================================ -->
+<script src="lib/js/jquery.nivo.slider.js"></script>
+<script src="lib/home.js"></script>
+<!-- meanmenu JS
+        ============================================ -->
+<script src="js/jquery.meanmenu.js"></script>
+<!-- owl.carousel JS
+        ============================================ -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- elevatezoom JS
+        ============================================ -->
+<script src="js/jquery.elevatezoom.js"></script>
+<!-- scrollUp JS
+        ============================================ -->
+<script src="js/jquery.scrollUp.min.js"></script>
+<!-- plugins JS
+        ============================================ -->
+<script src="js/plugins.js"></script>
+<!-- main JS
+        ============================================ -->
+<script src="js/main.js"></script>
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<!-- Bootstrap CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/font-awesome.min.css">
+<!-- owl.carousel CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/owl.carousel.css">
+<link rel="stylesheet" href="../css/owl.theme.css">
+<link rel="stylesheet" href="../css/owl.transitions.css">
+<!-- jquery-ui CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/jquery-ui.css">
+<!-- meanmenu CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/meanmenu.min.css">
+<!-- nivoslider CSS
+        ============================================ -->
+<link rel="stylesheet" href="../lib/css/nivo-slider.css">
+<link rel="stylesheet" href="../lib/css/preview.css">
+<!-- animate CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/animate.css">
+<!-- magic CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/magic.css">
+<!-- normalize CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/normalize.css">
+<!-- main CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/main.css">
+<!-- style CSS
+        ============================================ -->
+<link rel="stylesheet" href="../style.css">
+<!-- responsive CSS
+        ============================================ -->
+<link rel="stylesheet" href="../css/responsive.css">
+<!-- modernizr JS
+        ============================================ -->
+<script src="../js/vendor/modernizr-2.8.3.min.js"></script>
